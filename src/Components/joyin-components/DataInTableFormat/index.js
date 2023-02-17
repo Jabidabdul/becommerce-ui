a7b773a9f68a39ff7248ad7d91ddaaa16be8ad99
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-17 21:15:19 +0530

    table

---
+++ src/Components/joyin-components/DataInTableFormat/index.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+function TableComponent() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.body}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TableComponent;
