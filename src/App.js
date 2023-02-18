fcc3812f923854d55d156bfec15e79d7a1f65f77
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-02-18 22:53:00 +0530

    table

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { LoginProvider } from './ContextApi/LoginContext';
 import RoutePages from './RoutePages';
 import Footer from './Views/Footer';
 import Header from './Views/Header';
-
+import Table from './mumin/table'
 
 function App() {
   return (
     <div className="App">
       <LoginProvider>
         <Header />
-        <RoutePages />
+        {/* <RoutePages /> */}
+        <Table/>
         <Footer /> 
       </LoginProvider>
       
