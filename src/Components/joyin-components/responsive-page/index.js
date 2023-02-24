f5bb09b541bf59e74c843dcf354aaf91a4332c61
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-24 21:32:58 +0530

    responsive page

---
+++ src/Components/joyin-components/responsive-page/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.css"
+
+const index = () => {
+  return (
+    <div className="app">
+      <div className="first">
+        <h1>I am first</h1>
+      </div>
+      <div className="second">
+        <h1>I am second</h1>
+      </div>
+      <div className="third">
+        <h1>I am third</h1>
+      </div>
+      <div className="fourth">
+        <h1>I am fourth</h1>
+      </div>
+    </div>
+  )
+}
+
+export default index
