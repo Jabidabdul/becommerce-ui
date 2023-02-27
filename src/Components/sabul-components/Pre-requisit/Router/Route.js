efbba875dd4f3be944010708dc3f38316fffc1dc
Sabul Hussain Laskar <sabul673@gmail.com>
2023-02-27 23:32:18 +0530

    new

---
+++ src/Components/sabul-components/Pre-requisit/Router/Route.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css'
+// import SearchBar from './Components/sabul-components/SeacrhBar';
+import {Routes , Route} from 'react-router-dom';
+import { Home } from './Components/sabul-components/Home';
+import {About} from './Components/sabul-components/About';
+import {NavBar} from './Components/sabul-components/NavBar';
+import { OrderSummary } from './Components/sabul-components/OrderSummary';
+import { NoMatch } from './Components/sabul-components/NoMatch';
+import { Products } from './Components/sabul-components/Products';
+import { FeaturedProduct } from './Components/sabul-components/FeaturedProduct';
+import { NewProduct } from './Components/sabul-components/NewProduct';
+import {Index, Responsive} from './Components/sabul-components/Task-02/Index';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='OrderSummary' element={<OrderSummary/>} />
+    <Route path='*' element={<NoMatch/>} />
+    <Route path='Products' element={<Products/>}>
+      <Route index element={<FeaturedProduct/>} />
+      <Route path='FeaturedProducts' element={<FeaturedProduct/>} />
+      <Route path='NewProducts' element={<NewProduct/>} />
+    </Route>
+   </Routes>
+
+{/* Responsive page */}
+    <Responsive/>
+</>
+  );
+}
+
+export default App;
--- src/Components/sabul-components/Task-02/Index.js
+++ src/Components/sabul-components/Task-02/Index.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./Style.css";
 export const Responsive = ()=> {
     return(
-    <>
     <div className='Main-Div'>
     <div className='Div bg-1'>Div 1</div>
     <div className='Div bg-2'>Div 2</div>
     <div className='Div bg-3'>Div 3</div>
     <div className='Div bg-4'>Div 4</div>
    </div>
-   </>
     )
 }
